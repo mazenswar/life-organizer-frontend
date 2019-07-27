58beb76b3bb86a4bd831277c0cdaaf47621da176
Mazen Alswar <mazen.alswar@flatironschool.com>
2019-07-27 13:46:53 -0400

    inital commit

---
+++ src/API_CONSTANTS.js
@@ -0,0 +1,3 @@
+export const BASE_URL = "http://localhost:3000";
+export const YEAR_URL = "http://localhost:3000/years";
+export const MONTHS_URL = "http://localhost:3000/months";
--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import CalendarPages from "./Components/Calendar";
+import dates from "./data";
+import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  getMonth = monthName => {
+    this.setState({ month: monthName });
+  };
+  render() {
+    return (
+      <div className="App">
+        <CalendarPages.CalendarPage />
+      </div>
+    );
+  }
 }
 
 export default App;
